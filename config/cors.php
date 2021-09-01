4e488bbbe9fb7796bd1253e73b65011b91625790
monsur22 <monsurahmedshafiq@gmail.com>
2021-09-01 21:28:11 +0600

    cors problem

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    'exposed_headers' => ['Access-Control-Allow-Origin'],
     'max_age' => 0,
     'supports_credentials' => false,
 
